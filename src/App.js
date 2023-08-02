dae0243627906366c94a1d87fac328e2eeab1e4e
mrzasajad <mrzsajad@gmai.com>
2023-08-02 20:21:38 +0330

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header";
-import SideBar from "./components/SideBar";
-import Main from "./components/Main";
-import Test from "./components/Test";
+// import Header from "./components/Header";
+// import SideBar from "./components/SideBar";
+// import Main from "./components/Main";
+// import Test from "./components/Test";
 import Purchase from "./components/Purchase/Purchase";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
